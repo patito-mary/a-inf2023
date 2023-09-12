61413d70c0087ced9dbf065cb399708d1f6357c7
patito <mary.vergugo@userena.cl>
2023-09-12 13:19:07 -0300

    cosas de la clase de hoy jeje, aun no terminamos C :(

---
+++ controldeflujo1209.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int x = 1, y = 2;
+    if (x<=y) {
+        printf("x es mayor que y \n");
+    }
+    else {
+        printf("y es mayore que x \n");
+    }
+}
