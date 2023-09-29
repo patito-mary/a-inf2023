fc64888cc76633729352bc792a6cd7392f258354
patito <mary.vergugo@userena.cl>
2023-09-29 13:13:28 -0300

    clase 2909 viernes, clases con herencias

--- python/ejemplos/class2609.py
+++ python/ejemplos/class2609.py
@@ -2,7 +2,9 @@ class complex:
     def __init__(self, real, img):
         self.r = real
         self.i = img
+    def __str__(self):
+        return str(self.r)+'i'(self.i)
 
 x = complex(6,8)
-print(f'la parte real es {x.r}, y la imaginaria {x.i}')
+#print(f'la parte real es {x.r}, y la imaginaria {x.i}')
 #print(x.r, x.i)
